d4368745ad2e35ef7e39b435c8fd5a5709d61ee8
Iliass-R <iliass.rami.pro@gmail.com>
2022-10-01 19:25:40 +0200

    Initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class='p-5 max-w-4xl bg-black m-16'>
+
+    <!--  Résultat de la calculatrice -->
+    <div class='w-full rounded-full m-1 p-6 text-right leading-normal font-bold text-white bg-gray-800'>
+      {{ props.calculatorValue || 0 }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  calculatorValue: {
+    type: String,
+    required: true
+  }
+})
+
+
+</script>
